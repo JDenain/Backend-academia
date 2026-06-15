1ef3d86e1dca8db33ddf9c1b02b195d289b0f45a
Denain <denain@outlook.es>
2026-06-14 23:23:18 -0400

    departamentos autenticacion

--- src/routes/departamentos.routes.js
+++ src/routes/departamentos.routes.js
@@ -8,8 +8,8 @@ const router = Router();
 router.get('/api/departamentos', authenticateToken, getDepartamentos);
 
 // Solo el Administrador y root pueden crear, modificar o eliminar departamentos
-router.post('/api/departamentos', authenticateToken, authorize([1, 4]), createDepartamento);
-router.put('/api/departamentos/:id', authenticateToken, authorize([1, 4]), updateDepartamento);
-router.delete('/api/departamentos/:id', authenticateToken, authorize([1, 4]), deleteDepartamento);
+router.post('/api/departamentos', authenticateToken, authorize(1, 4), createDepartamento);
+router.put('/api/departamentos/:id', authenticateToken, authorize(1, 4), updateDepartamento);
+router.delete('/api/departamentos/:id', authenticateToken, authorize(1, 4), deleteDepartamento);
 
 export default router;
